7b9058681a3145e9c07cd9114805372827842a47
NoahE2005 <9014121@student.zadkine.nl>
2022-12-09 13:51:06 +0100

    a

---
+++ Noah/Portfolio.js
@@ -0,0 +1,8 @@
+//https://stackoverflow.com/questions/44846614/trigger-css-animations-in-javascript
+//https://codersblock.com/blog/creating-glow-effects-with-css/
+
+var text = "text";
+
+function Text(text) {
+    document.getElementById("BeginText").style.animation = "Begintext 5s 5s forward"
+}
