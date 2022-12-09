ed80cb43afa97c55c2bdeb8a05276a57898729cb
NoahE2005 <9014121@student.zadkine.nl>
2022-12-09 16:56:38 +0100

    a

--- Noah/Portfolio.js
+++ Noah/Portfolio.js
@@ -1,13 +1,16 @@
 //https://stackoverflow.com/questions/44846614/trigger-css-animations-in-javascript
 //https://codersblock.com/blog/creating-glow-effects-with-css/
 
-document.getElementById("TextChange").textContent = "he";
 
 function TextChange(text) {
-    document.getElementById('BeginText').style.color = "red";
-    document.getElementById('BeginText').innerHTML = text;
+    document.getElementById("BeginText").style.animation = "Begintext 5s 5s forward";
+    document.getElementById("BeginText").innerHTML = text;
 }
 
+function EindText() {
+    document.getElementsByClassName("PortfolioBubble").style.animation = "box-shadow-color 10s infinite linear";
+}
+EindText()
 
-TextChange("hello")
+TextChange(Hallo)
 document.getElementById("BeginText").innerHTML = "hello";
