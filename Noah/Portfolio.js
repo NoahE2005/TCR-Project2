eb9bf672992ebfef88773af4f0aea57a1afb8f80
NoahE2005 <9014121@student.zadkine.nl>
2022-12-12 13:22:10 +0100

    p

--- Noah/Portfolio.js
+++ Noah/Portfolio.js
@@ -3,14 +3,27 @@
 
 
 function TextChange(text) {
-    document.getElementById("BeginText").style.animation = "Begintext 5s 5s forward";
-    document.getElementById("BeginText").innerHTML = text;
+    document.getElementById("BeginText").innerText = text;
 }
 
-function EindText() {
-    document.getElementsByClassName("PortfolioBubble").style.animation = "box-shadow-color 10s infinite linear";
-}
-EindText()
 
-TextChange(Hallo)
-document.getElementById("BeginText").innerHTML = "hello";
+TextChange("Hallo")
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
