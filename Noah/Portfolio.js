a3d8ca5f76353d1c68ccc9b8d7df260019926d7b
NoahE2005 <9014121@student.zadkine.nl>
2022-12-15 14:23:09 +0100

    a

--- Noah/Portfolio.js
+++ Noah/Portfolio.js
@@ -7,24 +7,3 @@
 //https://moonfarmer.com
 //https://www.titusklein.com/?ref=hackernoon.com
 
-// Option 2 - jQuery Smooth Scrolling
-// $('.navbar a').on('click', function (e) {
-//   if (this.hash !== '') {
-//     e.preventDefault();
-//
- //    const hash = this.hash;
-//
-//     $('html, body')
- //      .animate({
-//        scrollTop: $(hash).offset().top
-//      },800);
-// }
- //});
-
-
-
- var _window = window,Splitting = _window.Splitting,ScrollOut = _window.ScrollOut;
-Splitting();
-ScrollOut({
-  targets: '.word',
-  scrollingElement: '.page' });
