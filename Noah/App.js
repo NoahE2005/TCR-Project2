78e807140d0073bac7bc6fed7be5855a76575638
NoahE2005 <9014121@student.zadkine.nl>
2022-12-20 11:40:38 +0100

    i

--- Noah/App.js
+++ Noah/App.js
@@ -17,7 +17,7 @@ const coins = Canvas.getContext("2d")
 const PlayerWalkSpeed = 3; //De speler snelheid
 const PlayerColor = 000000;
 const PlayerScaleXY = 20;
-const MaxCoints = 8;
+const MaxCoins = 8;
 
 var Breed = 0;
 var Hoog = 0;
@@ -62,14 +62,14 @@ function ColorPLayer() {
 }
 
 function CoinsBegin() {
-    for (let i = 0; i < MaxCoints; i++) {
+    for (let i = 0; i < MaxCoins; i++) {
         coinlocationsX.push(Math.floor(Math.random() * 500))
         coinlocationsY.push(Math.floor(Math.random() * 500))
     }
 }
 
 function Coins() {
-    for (let i = 0; i < MaxCoints; i++) {
+    for (let i = 0; i < MaxCoins; i++) {
       coins.beginPath();
         coins.fillRect(500,500, 30, 30)
         coins.fillStyle = "white"; //fix
