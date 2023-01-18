951ae924ed4e1449024b2cabc56112f4e7fd715e
NoahE2005 <9014121@student.zadkine.nl>
2023-01-18 09:20:10 +0100

    a

--- Homepagina.js
+++ Homepagina.js
@@ -79,8 +79,17 @@ function slideshow() {
 setTimeout(slideshow, 1000);
 var imageSlideshowInterval = setInterval(slideshow, 10000);
 
-function OpenUrl(Link) {
-  alert (currentImage)
-  alert (currentImage)
-  window.open(Link)
+function OpenUrl() {
+  if(counter ==1) {
+    window.open('Noah/noah-3.html')
+  }
+  if(counter == 2) {
+    window.open('Justin/justin-3.html')
+  }
+  if(counter == 3) {
+    window.open('Bjorn/bjorn-3.html')
+  }
+  if(counter == 4) {
+    window.open('Lars/lars-3.html')
+  }
 }
